9d155a873fb6f739b38b8b16f3f038ccf1a283f4
fdrigui <filipi.rigui@outlook.com>
2024-07-01 23:23:30 -0300

    Metade da aula 3

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,20 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'olá mundo!'}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', response_model=UserPublic, status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,26 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'email': 'test@test.com',
+            'password': 'password',
+        },
+    )
+
+    # Retornou Status Code Correto (201)?
+    assert response.status_code == HTTPStatus.CREATED
+
+    # Validar Schema de retorno UserPublic
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
