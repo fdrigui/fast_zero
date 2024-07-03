b14251c1c9de6d748c48dd004ab7bfcf35dc1e3e
fdrigui <filipi.rigui@outlook.com>
2024-07-02 23:25:50 -0300

    Adicionando rotas do CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,14 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
@@ -35,3 +27,106 @@ def test_create_user():
         'email': 'test@test.com',
         'id': 1,
     }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testusername',
+                'email': 'test@test.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_read_user_id_greather_than_DB_len(client):
+    response = client.get('/users/100')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_read_user_id_negative(client):
+    response = client.get('/users/-5')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'username': 'testuusername2',
+            'email': 'test2@test2.com',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'testuusername2',
+        'email': 'test2@test2.com',
+        'id': 1,
+    }
+
+
+def test_update_id_greather_than_DB_len(client):
+    response = client.put(
+        '/users/100',
+        json={
+            'password': '123',
+            'username': 'testuusername2',
+            'email': 'test2@test2.com',
+            'id': 100,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_update_id_negative(client):
+    response = client.put(
+        '/users/-5',
+        json={
+            'password': '123',
+            'username': 'testuusername2',
+            'email': 'test2@test2.com',
+            'id': -5,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_id_negative(client):
+    response = client.delete(
+        '/users/-5',
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_id_greather_than_DB_len(client):
+    response = client.delete(
+        '/users/100',
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
