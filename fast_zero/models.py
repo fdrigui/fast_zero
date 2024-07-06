1a2db40d44184d11db5f52f084686ac52a0d8d41
fdrigui <filipi.rigui@outlook.com>
2024-07-05 23:28:49 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -13,6 +13,9 @@ class User:
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
-    crreated_at: Mapped[datetime] = mapped_column(
+    created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, onupdate=func.now()
     )
